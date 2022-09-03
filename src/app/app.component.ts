7a2beb29dd773e5000851542ae25800933939a45
yousef <gheibiyousef2000@gmail.com>
2022-09-03 22:37:16 +0430

    complete this challenge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'multi';
+  rtlLangs = ['ar','morelangs'];
+
+  constructor(public translate: TranslateService) {  
+    translate.addLangs(['en', 'fa']);
+    translate.setDefaultLang('en');  
+ 
+}  
+  
+switchLang(lang: string) {  
+  this.translate.use(lang);  
+  document.getElementsByTagName("html")[0].setAttribute('lang', lang);
+} 
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';  
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';  
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +11,21 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,  
+    TranslateModule.forRoot({  
+      loader: {  
+         provide: TranslateLoader,  
+         useFactory: httpTranslateLoader,  
+         deps: [HttpClient]  
+         }  
+      })  
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+ // AOT compilation support  
+ export function httpTranslateLoader(http: HttpClient) {  
+  return new TranslateHttpLoader(http); }
